06e773df8425d0cb8651a16b0b62a5b69407f4bc
Robert Vilcu <robertvilcu89@gmail.com>
2025-03-08 23:56:17 +0000

    chore: add cashier necessary functionality

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Cashier\Cashier;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Cashier::calculateTaxes();
+
         Vite::prefetch(concurrency: 3);
     }
 }
