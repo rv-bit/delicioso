7707cd1fa6fce3c120e56cc3daf31f736015de41
Robert Vilcu <robertvilcu89@gmail.com>
2025-03-09 13:36:56 +0000

    feat: added react compiler for auto memo

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,29 @@
+
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const ReactCompilerConfig = {
+  target: '19' // '17' | '18' | '19'
+};
+
 export default defineConfig({
     plugins: [
+        react({
+            babel: {
+                plugins: [
+                    ["babel-plugin-react-compiler", ReactCompilerConfig],
+                ],
+            },
+        }),
+        tsconfigPaths(),
         tailwindcss(),
         laravel({
             input: 'resources/js/app.tsx',
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
-        react(),
-        tsconfigPaths()
     ],
 });
