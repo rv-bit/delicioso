a01601e6111e54f8004892765e5861165180f265
Robert Vilcu <robertvilcu89@gmail.com>
2025-03-11 20:37:45 +0000

    chore: refactor layout

--- resources/js/layouts/authenticated-layout.tsx
+++ resources/js/layouts/authenticated-layout.tsx
@@ -5,28 +5,20 @@ import { useMediaQuery } from "@/hooks/use-media-query";
 import NavLink from "@/components/NavLink";
 import { usePage } from "@inertiajs/react";
 
-export default function AuthenticatedLayout({
-	children,
-}: PropsWithChildren<{}>) {
+export default function AuthenticatedLayout({ children }: PropsWithChildren<{}>) {
 	const user = usePage().props.auth.user;
 	const isDesktop = useMediaQuery("(min-width: 768px)");
 
 	return (
 		<React.Fragment>
-			<nav className="bg-background h-full w-full">
+			<nav>
 				<div className="mx-auto max-w-7xl px-2">
 					<div className="my-2 flex h-10 items-center justify-start">
 						<div className="space-x-4">
-							<NavLink
-								href={route("dashboard")}
-								active={route().current("dashboard")}
-							>
+							<NavLink href={route("dashboard")} active={route().current("dashboard")}>
 								Dashboard
 							</NavLink>
-							<NavLink
-								href={route("profile.edit")}
-								active={route().current("profile.edit")}
-							>
+							<NavLink href={route("profile.edit")} active={route().current("profile.edit")}>
 								Profile Manage
 							</NavLink>
 						</div>
@@ -34,7 +26,7 @@ export default function AuthenticatedLayout({
 				</div>
 			</nav>
 
-			<main>{children}</main>
+			{children}
 		</React.Fragment>
 	);
 }
