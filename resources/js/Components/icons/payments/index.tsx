8ae8d11711e26f5d4f214991e7614c2e0b0f8eb3
Robert Vilcu <robertvilcu89@gmail.com>
2025-03-12 15:53:01 +0000

    chore: moved icons as react components

---
+++ resources/js/Components/icons/payments/index.tsx
@@ -0,0 +1,5 @@
+export * from "./amex-pay";
+export * from "./apple-pay";
+export * from "./master-pay";
+export * from "./paypal-pay";
+export * from "./visa-pay";
---
+++ resources/js/Components/icons/socials/instagram.tsx
@@ -0,0 +1,40 @@
+import { SVGAttributes } from "react";
+
+export function InstagramIcon(props: SVGAttributes<SVGElement>) {
+	return (
+		<svg {...props} viewBox="0 0 800 800" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path
+				d="M561.676 0H238.324C106.912 0 0 106.912 0 238.324V561.676C0 693.088 106.912 800 238.324 800H561.676C693.088 800 800 693.088 800 561.676V238.324C799.999 106.912 693.087 0 561.676 0ZM719.52 561.676C719.52 648.85 648.85 719.52 561.676 719.52H238.324C151.15 719.52 80.48 648.85 80.48 561.676V238.324C80.48 151.149 151.15 80.48 238.324 80.48H561.676C648.85 80.48 719.52 151.149 719.52 238.324V561.676Z"
+				fill="url(#paint0_linear_9_14)"
+			/>
+			<path
+				d="M400 193.092C285.911 193.092 193.092 285.91 193.092 399.999C193.092 514.087 285.911 606.907 400 606.907C514.09 606.907 606.908 514.088 606.908 399.999C606.908 285.909 514.09 193.092 400 193.092ZM400 526.429C330.175 526.429 273.572 469.825 273.572 400C273.572 330.175 330.176 273.572 400 273.572C469.825 273.572 526.429 330.175 526.429 400C526.429 469.824 469.824 526.429 400 526.429Z"
+				fill="url(#paint1_linear_9_14)"
+			/>
+			<path
+				d="M607.303 244.226C634.685 244.226 656.881 222.029 656.881 194.648C656.881 167.267 634.685 145.07 607.303 145.07C579.922 145.07 557.725 167.267 557.725 194.648C557.725 222.029 579.922 244.226 607.303 244.226Z"
+				fill="url(#paint2_linear_9_14)"
+			/>
+			<defs>
+				<linearGradient id="paint0_linear_9_14" x1="400" y1="797.669" x2="400" y2="6.21344" gradientUnits="userSpaceOnUse">
+					<stop stop-color="#E09B3D" />
+					<stop offset="0.3" stop-color="#C74C4D" />
+					<stop offset="0.6" stop-color="#C21975" />
+					<stop offset="1" stop-color="#7024C4" />
+				</linearGradient>
+				<linearGradient id="paint1_linear_9_14" x1="400" y1="797.669" x2="400" y2="6.21349" gradientUnits="userSpaceOnUse">
+					<stop stop-color="#E09B3D" />
+					<stop offset="0.3" stop-color="#C74C4D" />
+					<stop offset="0.6" stop-color="#C21975" />
+					<stop offset="1" stop-color="#7024C4" />
+				</linearGradient>
+				<linearGradient id="paint2_linear_9_14" x1="607.303" y1="797.669" x2="607.303" y2="6.21352" gradientUnits="userSpaceOnUse">
+					<stop stop-color="#E09B3D" />
+					<stop offset="0.3" stop-color="#C74C4D" />
+					<stop offset="0.6" stop-color="#C21975" />
+					<stop offset="1" stop-color="#7024C4" />
+				</linearGradient>
+			</defs>
+		</svg>
+	);
+}
