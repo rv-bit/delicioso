8bd725eb6f4fbb523c4476c698959a9ec7094c77
Robert Vilcu <robertvilcu89@gmail.com>
2025-03-18 00:36:05 +0000

    chore: auto refactor

--- resources/js/Components/ui/accordion.tsx
+++ resources/js/Components/ui/accordion.tsx
@@ -4,30 +4,15 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-function Accordion({
-	...props
-}: React.ComponentProps<typeof AccordionPrimitive.Root>) {
+function Accordion({ ...props }: React.ComponentProps<typeof AccordionPrimitive.Root>) {
 	return <AccordionPrimitive.Root data-slot="accordion" {...props} />;
 }
 
-function AccordionItem({
-	className,
-	...props
-}: React.ComponentProps<typeof AccordionPrimitive.Item>) {
-	return (
-		<AccordionPrimitive.Item
-			data-slot="accordion-item"
-			className={cn("border-b last:border-b-0", className)}
-			{...props}
-		/>
-	);
+function AccordionItem({ className, ...props }: React.ComponentProps<typeof AccordionPrimitive.Item>) {
+	return <AccordionPrimitive.Item data-slot="accordion-item" className={cn("border-b last:border-b-0", className)} {...props} />;
 }
 
-function AccordionTrigger({
-	className,
-	children,
-	...props
-}: React.ComponentProps<typeof AccordionPrimitive.Trigger>) {
+function AccordionTrigger({ className, iconClassName, children, ...props }: React.ComponentProps<typeof AccordionPrimitive.Trigger> & { iconClassName?: string }) {
 	return (
 		<AccordionPrimitive.Header className="flex">
 			<AccordionPrimitive.Trigger
@@ -39,23 +24,15 @@ function AccordionTrigger({
 				{...props}
 			>
 				{children}
-				<ChevronDownIcon className="text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200" />
+				<ChevronDownIcon className={cn("text-muted-foreground pointer-events-none size-4 shrink-0 transition-transform duration-200", iconClassName)} />
 			</AccordionPrimitive.Trigger>
 		</AccordionPrimitive.Header>
 	);
 }
 
-function AccordionContent({
-	className,
-	children,
-	...props
-}: React.ComponentProps<typeof AccordionPrimitive.Content>) {
+function AccordionContent({ className, children, ...props }: React.ComponentProps<typeof AccordionPrimitive.Content>) {
 	return (
-		<AccordionPrimitive.Content
-			data-slot="accordion-content"
-			className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm"
-			{...props}
-		>
+		<AccordionPrimitive.Content data-slot="accordion-content" className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm" {...props}>
 			<div className={cn("pt-0 pb-4", className)}>{children}</div>
 		</AccordionPrimitive.Content>
 	);
