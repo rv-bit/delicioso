1d79749b26bff081e82c17ae5c5c50d7605a1769
Robert Vilcu <robertvilcu89@gmail.com>
2025-03-18 21:56:32 +0000

    fix: display message when no admin components can be shown

--- resources/js/routes/profile/admin/dashboard.tsx
+++ resources/js/routes/profile/admin/dashboard.tsx
@@ -31,15 +31,21 @@ export default function Dashboard({ products, prices }: { products: Product[]; p
 		[products, prices],
 	);
 
+	const componentItemsWhichAreHidden = componentItems.filter((item) => item.isHidden);
+
 	return (
 		<RootLayout>
 			<AuthenticatedLayout>
 				<Head title="Dashboard" />
 
 				<div className="mx-auto flex max-w-7xl flex-col gap-2 px-2">
-					<div className="overflow-hidden rounded-sm bg-white shadow-sm dark:bg-gray-800">
-						<div className="p-6 text-gray-900 dark:text-gray-100">You're logged in!, and found the admin dashboard</div>
-					</div>
+					{componentItemsWhichAreHidden.length === componentItems.length && (
+						<div className="flex max-h-[50rem] flex-col gap-3 overflow-auto bg-white p-5 shadow-sm">
+							<header>
+								<h1 className="text-lg font-medium text-gray-900 dark:text-gray-100">No Items Available</h1>
+							</header>
+						</div>
+					)}
 
 					{componentItems.map((item, index) => {
 						if (item.isHidden) return null;
