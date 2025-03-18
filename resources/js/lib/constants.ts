9747053cefe8ab1b43abe5bbc5c9e7afd10a0ad9
Robert Vilcu <robertvilcu89@gmail.com>
2025-03-18 00:38:09 +0000

    chore: add constants and its types

--- resources/js/lib/constants.ts
+++ resources/js/lib/constants.ts
@@ -1,1 +1,9 @@
 export const APP_NAME = import.meta.env.VITE_APP_NAME;
+
+export const PRICE_CURRENCIES = ["GBP", "USD", "EUR"];
+export enum PriceCurrencyEnum {
+	GBP = "GBP",
+	USD = "USD",
+	EUR = "EUR",
+}
+export type PriceCurrency = (typeof PRICE_CURRENCIES)[number];
