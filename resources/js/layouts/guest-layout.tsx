8539193ad70654571cd6a4ea43e20dba52d1bb38
Robert Vilcu <robertvilcu89@gmail.com>
2025-03-19 21:17:49 +0000

    chore: refactor page to take in the new app name

--- resources/js/layouts/guest-layout.tsx
+++ resources/js/layouts/guest-layout.tsx
@@ -1,7 +1,7 @@
 import { Link } from "@inertiajs/react";
 import { PropsWithChildren } from "react";
 
-import ApplicationLogo from "@/components/icons/ApplicationLogo";
+import { APP_NAME } from "@/lib/constants";
 
 export default function Guest({ children }: PropsWithChildren) {
 	return (
@@ -15,8 +15,8 @@ export default function Guest({ children }: PropsWithChildren) {
 			}}
 			className="flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900"
 		>
-			<Link href="/" name="homepage" aria-label="Go To Homepage">
-				<ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
+			<Link href="/" name="homepage" aria-label="Go To Homepage" className="font-bricolage text-3xl font-semibold -tracking-wide lowercase italic">
+				{APP_NAME}
 			</Link>
 
 			<div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg dark:bg-gray-800">{children}</div>
