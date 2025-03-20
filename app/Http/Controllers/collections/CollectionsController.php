cde1d43ae584cdf37d3367f62de906b6c531e508
Robert Vilcu <robertvilcu89@gmail.com>
2025-03-20 15:11:03 +0000

    feat: added a default collections page, where all the categories can be found for the user

--- app/Http/Controllers/collections/CollectionsController.php
+++ app/Http/Controllers/collections/CollectionsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\collections;
 
+use App\Enum\CategoriesEnum;
 use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
@@ -13,15 +14,18 @@ use Laravel\Cashier\Cashier;
 
 class CollectionsController extends Controller
 {
-    public function index(Request $request, $any = null): Response
+    public function index(Request $request, $any = null)
     {
         switch ($any) {
             case null:
                 // means no path, just the base url, so we can return the default view
                 return Inertia::render('collections/index');
-            case 'something':
-                return Inertia::render('collections/index', [
-                    'category' => 'something'
+            case 'sharing_boxes':
+            case 'breakfast':
+            case 'lunch':
+            case 'sweets_cakes':
+                return Inertia::render('collections/products/index', [
+                    'category' => CategoriesEnum::from($any)->label(),
                 ]);
             default:
                 // if the path is not found, we can return an exception to 404, since we already handle the page there
--- resources/js/routes/collections/index.tsx
+++ resources/js/routes/collections/index.tsx
@@ -1,171 +1,66 @@
-import { Head, Link } from "@inertiajs/react";
-import React from "react";
+import { Head, Link, usePage } from "@inertiajs/react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
 import { cn } from "@/lib/utils";
 
 import RootLayout from "@/layouts/root-layout";
 
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
-import { Checkbox } from "@/components/ui/checkbox";
-
 interface ItemData {
 	title: string;
 	img: string;
-	imgPreview?: string;
-	calories?: number;
-	price?: number;
 }
 
-const filters = [
-	{
-		title: "All",
-		children: ["New Arrivals", "Best Sellers"],
-		defaultOpen: true,
-	},
-	{
-		title: "Price",
-		children: ["Under $50", "$50 - $100", "$100 - $200", "$200 & Above"],
-		defaultOpen: true,
-	},
-	{
-		title: "Serves",
-		children: ["1 - 2", "3 - 4", "5 - 6", "7 & Above"],
-		defaultOpen: false,
-	},
-];
-
 export default function Collections({ category }: { category: string }) {
-	const [selectedFilter, setSelectedFilter] = React.useState<string[]>([]);
+	const categories = usePage().props.categories.labels;
+	const categoriesImages = usePage().props.categories.images;
+
+	console.log(categories, categoriesImages);
 
 	return (
 		<RootLayout footer={true}>
 			<Head title="Collections" />
 
 			<section className="mx-auto mt-20 flex max-w-7xl flex-col items-start justify-start gap-5 px-2 md:px-5">
-				<section className="flex flex-col">
-					<h1 className="text-4xl font-semibold text-black">{category}</h1>
-					<p className="text-lg font-medium text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				<section className="flex w-full flex-col gap-1">
+					<h1 className="text-4xl font-semibold text-black">Collections</h1>
+					<span className="max-sm:no-scrollbar flex h-auto max-sm:overflow-x-scroll">
+						{Object.keys(categories).map((category, index) => (
+							<Link
+								key={index}
+								href={`/collections/${category}`}
+								className="mr-2 flex items-center justify-center rounded-md bg-gray-200 px-3 py-1.5 text-sm font-medium whitespace-nowrap text-black transition-colors duration-150 ease-linear hover:bg-gray-300"
+							>
+								{categories[category]}
+							</Link>
+						))}
+					</span>
 				</section>
 
 				<div className="flex h-full w-full gap-5">
-					<aside id="filters-section" className="sticky top-25 flex h-full min-h-90 min-w-60 flex-col gap-4 max-lg:hidden">
-						<span>
-							<h1 className="text-xl font-medium text-black">Filter:</h1>
-							<hr className="border-border mt-2 h-1 w-full" />
-						</span>
+					<div className="grid w-full grid-cols-2 gap-2 space-y-4">
+						{Object.keys(categories).map((category, index) => (
+							<Link key={index} href={`/collections/${category}`} className={cn("group flex h-auto w-full flex-col items-start justify-start")}>
+								<div className="relative w-full overflow-hidden rounded-xs">
+									<LazyLoadImage
+										key={`main-${index}`}
+										src={categoriesImages[category]}
+										alt="main-image-tab"
+										effect="opacity"
+										width={"100%"}
+										className="h-full w-full object-cover opacity-100 transition-all duration-150 ease-linear group-hover:scale-110"
+									/>
+								</div>
 
-						{filters.map((filter, index) => (
-							<React.Fragment key={index}>
-								<Accordion type="single" collapsible defaultValue={filter.defaultOpen ? filter.title : undefined} className="h-fit w-full">
-									<AccordionItem key={index} value={filter.title} className="flex w-full flex-col items-start justify-start gap-2">
-										<AccordionTrigger containerClassName="flex w-full" className="flex w-full items-center justify-between gap-1 p-0">
-											<h1 className="text-md tracking-tight text-black">{filter.title}</h1>
-										</AccordionTrigger>
-
-										<AccordionContent>
-											<ul className="flex flex-col items-start gap-1">
-												{filter.children.map((child, index) => (
-													<li key={index} className="flex items-center justify-center gap-2">
-														<Checkbox
-															value={child}
-															title={child}
-															checked={selectedFilter.includes(child)}
-															onCheckedChange={(checked) => {
-																if (checked) {
-																	setSelectedFilter([...selectedFilter, child]);
-																} else {
-																	setSelectedFilter(selectedFilter.filter((filter) => filter !== child));
-																}
-															}}
-															className="size-6 rounded-xs"
-														/>
-														<label htmlFor={child} className="text-md leading-none font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-															{child}
-														</label>
-													</li>
-												))}
-											</ul>
-										</AccordionContent>
-									</AccordionItem>
-								</Accordion>
-
-								{index !== filters.length - 1 && <hr className="border-border h-1 w-full" />}
-							</React.Fragment>
+								<span className="mt-1 flex w-full flex-col items-start text-wrap break-all">
+									<h1 className="text-left text-[0.890rem] leading-6 font-medium tracking-wider text-black group-hover:underline group-hover:decoration-1 md:text-lg">
+										{categories[category]}
+									</h1>
+								</span>
+							</Link>
 						))}
-					</aside>
-					<div className="no-scrollbar grid w-full grid-cols-4 gap-3 overflow-y-visible max-lg:grid-cols-3 max-sm:grid-cols-2">
-						<ListItem item={{ title: "6 HOT CROSS BUNSdwadwdawdwadwadwawadwadwadwa", img: "/media/landing/items/6_hot_cross_buns.webp", calories: 250, price: 10.24 }} index={0} />
-						<ListItem item={{ title: "6 HOT CROSS BUNS", img: "/media/landing/items/6_hot_cross_buns.webp", calories: 250, price: 10.24 }} index={0} />
-						<ListItem item={{ title: "6 HOT CROSS BUNS", img: "/media/landing/items/6_hot_cross_buns.webp", calories: 250, price: 10.24 }} index={0} />
-						<ListItem item={{ title: "6 HOT CROSS BUNS", img: "/media/landing/items/6_hot_cross_buns.webp", calories: 250, price: 10.24 }} index={0} />
-						<ListItem item={{ title: "6 HOT CROSS BUNS", img: "/media/landing/items/6_hot_cross_buns.webp", calories: 250, price: 10.24 }} index={0} />
-						<ListItem item={{ title: "6 HOT CROSS BUNS", img: "/media/landing/items/6_hot_cross_buns.webp", calories: 250, price: 10.24 }} index={0} />
-						<ListItem item={{ title: "6 HOT CROSS BUNS", img: "/media/landing/items/6_hot_cross_buns.webp", calories: 250, price: 10.24 }} index={0} />
-						<ListItem item={{ title: "6 HOT CROSS BUNS", img: "/media/landing/items/6_hot_cross_buns.webp", calories: 250, price: 10.24 }} index={0} />
-						<ListItem item={{ title: "6 HOT CROSS BUNS", img: "/media/landing/items/6_hot_cross_buns.webp", calories: 250, price: 10.24 }} index={0} />
-						<ListItem item={{ title: "6 HOT CROSS BUNS", img: "/media/landing/items/6_hot_cross_buns.webp", calories: 250, price: 10.24 }} index={0} />
-						<ListItem item={{ title: "6 HOT CROSS BUNS", img: "/media/landing/items/6_hot_cross_buns.webp", calories: 250, price: 10.24 }} index={0} />
-						<ListItem item={{ title: "6 HOT CROSS BUNS", img: "/media/landing/items/6_hot_cross_buns.webp", calories: 250, price: 10.24 }} index={0} />
-						<ListItem item={{ title: "6 HOT CROSS BUNS", img: "/media/landing/items/6_hot_cross_buns.webp", calories: 250, price: 10.24 }} index={0} />
-						<ListItem item={{ title: "6 HOT CROSS BUNS", img: "/media/landing/items/6_hot_cross_buns.webp", calories: 250, price: 10.24 }} index={0} />
-						<ListItem item={{ title: "6 HOT CROSS BUNS", img: "/media/landing/items/6_hot_cross_buns.webp", calories: 250, price: 10.24 }} index={0} />
 					</div>
 				</div>
 			</section>
 		</RootLayout>
-	);
-}
-
-export function ListItem({ className, item, index }: { className?: string; item: ItemData; index: number }) {
-	const [hovered, setHovered] = React.useState(false);
-
-	const canHover = React.useMemo(() => {
-		return item.imgPreview !== undefined && hovered;
-	}, [item.imgPreview, hovered]);
-
-	return (
-		<Link
-			href="/"
-			className={cn("group flex w-full flex-col items-start justify-start", className)}
-			onMouseEnter={() => {
-				setHovered(true);
-			}}
-			onMouseLeave={() => {
-				setHovered(false);
-			}}
-		>
-			<div className="relative w-full overflow-hidden rounded-xs">
-				<LazyLoadImage
-					key={`main-${index}`}
-					src={item.img}
-					alt="main-image-tab"
-					height="100%"
-					width="100%"
-					effect="opacity"
-					className={`group-hover:scale-105" h-full w-full object-cover opacity-100 transition-all duration-350 ease-linear`}
-					style={{ opacity: canHover ? 0 : 1 }}
-				/>
-
-				{item.imgPreview && (
-					<LazyLoadImage
-						key={`preview-${index}`}
-						src={item.imgPreview}
-						alt="preview-image-tab"
-						height="100%"
-						width="100%"
-						effect="opacity"
-						className={`h-full w-full object-cover opacity-0 transition-all duration-350 ease-linear group-hover:scale-105`}
-						style={{ opacity: hovered ? 1 : 0 }}
-					/>
-				)}
-			</div>
-
-			<span className="mt-1 flex w-full flex-col items-start text-wrap break-all">
-				<h1 className="text-left text-[0.890rem] leading-6 font-medium tracking-wider text-black group-hover:underline group-hover:decoration-1 md:text-lg">{item.title}</h1>
-				{item.calories && <p className="text-left font-sans text-[0.750rem] font-medium text-gray-600 md:-mb-1 md:text-xs">{item.calories} kcal</p>}
-				{item.price && <p className="text-md md:text-md text-left text-[0.850rem] font-medium tracking-wider text-black/70 md:leading-7">${item.price}</p>}
-			</span>
-		</Link>
 	);
 }
