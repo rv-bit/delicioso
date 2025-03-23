a498443db87de5c4677c82bdd49b7ae550b72276
Robert Vilcu <robertvilcu89@gmail.com>
2025-03-23 20:49:29 +0000

    feat: added 2 formatting functions

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export function getCurrencySymbol(currencyCode: string) {
+	const parts = new Intl.NumberFormat(undefined, { style: "currency", currency: currencyCode }).formatToParts(0);
+
+	const symbol = parts.find((part) => part.type === "currency");
+	return symbol ? symbol.value : currencyCode;
+}
+
+export function format(number: number, currency: string) {
+	return new Intl.NumberFormat(undefined, {
+		style: "currency",
+		currency,
+		currencyDisplay: "symbol",
+	})
+		.format(number)
+		.replace(currency, "")
+		.trim();
+}
