0c95ad976e4d659a5840ee14b99505d01845f6dc
Robert Vilcu <robertvilcu89@gmail.com>
2025-03-24 23:34:43 +0000

    chore: removed everything from dashboard, orders / receipts table will come in hre

--- resources/js/routes/profile/Dashboard.tsx
+++ resources/js/routes/profile/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Head, Link, usePage } from "@inertiajs/react";
+import { Head, usePage } from "@inertiajs/react";
 
 import AuthenticatedLayout from "@/layouts/authenticated-layout";
 import RootLayout from "@/layouts/root-layout";
@@ -11,23 +11,7 @@ export default function Dashboard() {
 			<AuthenticatedLayout>
 				<Head title="Dashboard" />
 
-				<div className="mx-auto flex max-w-7xl flex-col gap-2 px-2">
-					<div className="overflow-hidden rounded-sm bg-white shadow-sm">
-						<div className="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
-					</div>
-
-					<div className="overflow-hidden rounded-sm bg-white shadow-sm">
-						<div className="w-full p-6 text-gray-900 dark:text-gray-100">
-							<Link
-								href={route("payment.checkout", {
-									product: "price_1R4luvIv1F2scOeLrcK4LC8D",
-								})}
-							>
-								Test Checkout
-							</Link>
-						</div>
-					</div>
-				</div>
+				<div className="mx-auto flex max-w-7xl flex-col gap-2 px-2"></div>
 			</AuthenticatedLayout>
 		</RootLayout>
 	);
